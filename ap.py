9b5abf83feda98c8e9e1ea3e5075c37602d72584
ELI <elikiprop42@gmail.com>
2023-09-18 22:48:13 +0300

    initialize my project

---
+++ ap.py
@@ -0,0 +1,11 @@
+is_hot = False
+is_cold = False
+if is_hot:
+    print("it's a hot day")
+    print("drink a lot of water")
+elif is_cold:
+    print("it's a cold day")
+    print("wear warm clothes")
+else:
+    print("it's a lovely day")
+    print("Enjoy your day")
